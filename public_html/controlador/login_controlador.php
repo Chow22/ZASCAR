978065b1660374f85f27a3be2a0c68de539780de
jennerys13 <jennsflow19@gmail.com>
2017-12-20 10:39:26 +0100

    login y  boton de login en inicio

---
+++ public_html/controlador/login_controlador.php
@@ -0,0 +1,14 @@
+<?php
+require_once("../modelo/login.php");
+$usuario = filter_input(INPUT_POST, 'usuario');
+$pass = filter_input(INPUT_POST, 'pass');
+$login = new Login();
+$pd = $login->comprobar_login($usuario, $pass);
+if ($pd == NULL) {
+    echo 'ERROR, VUELVA A INTENTARLO CON UN USUARIO VÁLIDO';
+     echo ' <br>';
+    echo ' <a href="../vista/login.php">Volver</a>';
+}else{
+    header("Location: ../controlador/usuario_controlador.php");
+}
+?>
