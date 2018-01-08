24b6ff1c34387bf086f6be16d8f8fa0beb72cf0a
Kiwano-sama <zharkenheim@gmail.com>
2018-01-08 10:04:08 +0100

    cambio de html a php en index para includes y footer.php

---
+++ public_html/footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <p>&copy; Puedes contactar con nosotros en el siguiente enlace | <a href="/" target="_blank" rel="nofollow">Contacto</a></p>
+            <address>
+                Contact: <a href="mailto:ethazi3@gmail.com">Contacta vía E-mail</a>
+            </address>
+        </footer>   
