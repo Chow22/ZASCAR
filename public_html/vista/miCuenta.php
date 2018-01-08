d185e63cb1729a09ae51683469cf458bc9f61753
asier <asier@192.168.32.119>
2018-01-08 10:23:34 +0100

    varios

--- public_html/vista/miCuenta.php
+++ public_html/vista/miCuenta.php
@@ -5,16 +5,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
         <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="../js/jquery-3.2.1.min.js" type="text/javascript"></script>
 
         <!-- Popper JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
 
         <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script> 
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
         <link href="../css/styloMiCuenta.css" rel="stylesheet" type="text/css"/>
         <script src="../js/scriptMiCuenta.js" type="text/javascript"></script>
     </head>
@@ -31,14 +31,14 @@
        
 
                             <a target="_blank" href="paris.jpg">
-                                <img src="http://bambinoides.com/wp-content/uploads/2014/10/Selfi_monalisa.jpg" alt="Paris" style="width:25%" align="right">
+                                <img src="../img/Selfi_monalisa.jpg"  alt="Paris" style="width:25%" align="right">
                             </a>
                           
                             <!-- Nombre-->
                             <div class="form-group">
                                 <label class=" control-label" for="textinput">Nombre</label>  
                                 <div class="">
-                                    <input id="nombre" name="textinput" placeholder="nombre.." class="form-control input-md" type="text" required>
+                                    <input id="nombre" name="textinput" placeholder="" class="form-control input-md" type="text" required>
                                 </div>
                             </div>
 
@@ -46,28 +46,28 @@
                             <div class="form-group">
                                 <label class=" control-label" for="textinput">Apellido</label>  
                                 <div class="">
-                                    <input id="apellido" name="textinput" placeholder="apellido.." class="form-control input-md" type="text" required>
+                                    <input id="apellido" name="textinput" placeholder="" class="form-control input-md" type="text" required>
                                 </div>
                             </div>
                             <!-- Telefono-->
                             <div class="form-group">
                                 <label class=" control-label" for="textinput">Telefono</label>  
                                 <div class="">
-                                    <input id="telefono" type="tel" name="textinput" placeholder="telefono.." class="form-control input-md" type="text" required>
+                                    <input id="telefono" type="tel" name="textinput" placeholder="" class="form-control input-md" type="text" required>
                                 </div>
                             </div>
                             <!-- Email-->
                             <div class="form-group">
                                 <label class=" control-label" for="textinput">Email</label>  
                                 <div class="">
-                                    <input id="textinput" name=email" type="email" placeholder="email.." class="form-control input-md" type="text" required>
+                                    <input id="textinput" name=email" type="email" placeholder="" class="form-control input-md" type="text" required>
                                 </div>
                             </div>
                             <!-- Coche-->
                             <div class="form-group">
                                 <label class=" control-label" for="textinput">Coche</label>  
                                 <div class="">
-                                    <input id="textinput" name="textinput" placeholder="coche.." class="form-control input-md" type="text" ><button id="singlebutton" name="singlebutton" class="btn btn-default">Añadir</button>
+                                    <input id="textinput" name="textinput" placeholder="" class="form-control input-md" type="text" ><button id="singlebutton" name="singlebutton" class="btn btn-default">Añadir</button>
                                 </div>
                             </div>
                             <button type="submit" id="singlebutton" name="singlebutton" class="btn btn-default">Modificar</button>
@@ -91,34 +91,11 @@
                             </div>
                             <!-- Trayectos peticiones -->
                             <div class="form-group">
-                                <label class="" for="selectmultiple">Trayectos peticiones</label>
+                                <label class="" for="selectmultiple">Peticiones</label>
                                 <div class="scroll">
-                                    <table>
-                                        <tr>
-                                            <td><strong>Curso</strong></td>
-                                            <td><strong>Horas</strong></td>
-                                            <td><strong>Horario</strong></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>CSS</td>
-                                            <td>20</td>
-                                            <td>16:00 - 20:00</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>HTML</td>
-                                            <td>20</td>
-                                            <td>16:00 - 20:00</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>Dreamweaver</td>
-                                            <td>60</td>
-                                            <td>16:00 - 20:00</td>
-                                        </tr>
-                                    </table>
-                                </div>
+                                      <div id="tablaPeticiones">
+                                        
+                                    </div>
 
                             </div>
 
