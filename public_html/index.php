27c37c182cf816918bce1302da493df58b88c72a
Kiwano-sama <zharkenheim@gmail.com>
2018-01-09 09:49:18 +0100

    cambio css al de Galán que es más bonico

--- public_html/index.php
+++ public_html/index.php
@@ -18,18 +18,18 @@
     <body>
         <header>                         
             <div id="logo"><img src="img/logo.png">ZASCAR Enterprises
-             <a  href="vista/login.php"><img class="login-img" src="img/loginbutton.png" alt=""/></a>
+                <a  href="vista/login.php"><img class="login-img" src="img/loginbutton.png" alt=""/></a>
             </div>
             <br>
-             <br>
+            <br>
             <nav>  
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="vista/miCuenta.html">MiCuenta</a></li>                                                        
+                    <li><a href="vista/miCuenta.php">MiCuenta</a></li>                                                        
                     <li><a href="http://html-css-js.com/css/code/">CSS</a></li>
                     <li><a href="http://htmlcheatsheet.com/js/">JS</a></li>
                 </ul>
-               
+
             </nav>
         </header>
         <section>
@@ -38,126 +38,125 @@
         <section id="pageContent">
             <main role="main">
                 <section class="section-white">
-  <div class="container">
+                    <div class="container">
 
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-            <img src="img/carousel0.png" alt="...">
-          <div class="carousel-caption">
-            <h2>Heading</h2>
-          </div>
-        </div>
-        <div class="item">
-            <img src="img/carousel1.jpg" alt="...">
-          <div class="carousel-caption">
-            <h2>Heading</h2>
-          </div>
-        </div>
-        <div class="item">
-            <img src="img/carousel2.jpg" alt="...">
-          <div class="carousel-caption">
-            <h2>Heading</h2>
-          </div>
-        </div>
-      </div>
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img src="img/carousel0.png" alt="...">
+                                    <div class="carousel-caption">
+                                        <h2>Heading</h2>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <img src="img/carousel1.jpg" alt="...">
+                                    <div class="carousel-caption">
+                                        <h2>Heading</h2>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <img src="img/carousel2.jpg" alt="...">
+                                    <div class="carousel-caption">
+                                        <h2>Heading</h2>
+                                    </div>
+                                </div>
+                            </div>
 
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
+                            <!-- Controls -->
+                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>
 
-  </div>
-</section>
+                    </div>
+                </section>
             </main>
             <aside>
-               <div id="wrapper">
+                <div id="wrapper">
 
-	<ul class="menu">
-		<li class="item1"><a href="#">Friends</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Strange “Stuff”</a></li>
-				<li class="subitem3"><a href="#">Automatic Fails</a></li>
-			</ul>
-		</li>
-		<li class="item2"><a href="#">Videos</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Strange “Stuff”</a></li>
-				<li class="subitem3"><a href="#">Automatic Fails</a></li>
-			</ul>
-		</li>
-		<li class="item3"><a href="#">Galleries</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Strange “Stuff”</a></li>
-				<li class="subitem3"><a href="#">Automatic Fails</a></li>
-			</ul>
-		</li>
-		<li class="item4"><a href="#">Podcasts</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Strange “Stuff”</a></li>
-				<li class="subitem3"><a href="#">Automatic Fails</a></li>
-			</ul>
-		</li>
-		<li class="item5"><a href="#">Robots</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Strange “Stuff”</a></li>
-				<li class="subitem3"><a href="#">Automatic Fails</a></li>
-			</ul>
-		</li>
-	</ul>
+                    <ul class="menu">
+                        <li class="item1"><a href="#">Friends</a>
+                            <ul>
+                                <li class="subitem1"><a href="#">Cute Kittens</a></li>
+                                <li class="subitem2"><a href="#">Strange “Stuff”</a></li>
+                                <li class="subitem3"><a href="#">Automatic Fails</a></li>
+                            </ul>
+                        </li>
+                        <li class="item2"><a href="#">Videos</a>
+                            <ul>
+                                <li class="subitem1"><a href="#">Cute Kittens</a></li>
+                                <li class="subitem2"><a href="#">Strange “Stuff”</a></li>
+                                <li class="subitem3"><a href="#">Automatic Fails</a></li>
+                            </ul>
+                        </li>
+                        <li class="item3"><a href="#">Galleries</a>
+                            <ul>
+                                <li class="subitem1"><a href="#">Cute Kittens</a></li>
+                                <li class="subitem2"><a href="#">Strange “Stuff”</a></li>
+                                <li class="subitem3"><a href="#">Automatic Fails</a></li>
+                            </ul>
+                        </li>
+                        <li class="item4"><a href="#">Podcasts</a>
+                            <ul>
+                                <li class="subitem1"><a href="#">Cute Kittens</a></li>
+                                <li class="subitem2"><a href="#">Strange “Stuff”</a></li>
+                                <li class="subitem3"><a href="#">Automatic Fails</a></li>
+                            </ul>
+                        </li>
+                        <li class="item5"><a href="#">Robots</a>
+                            <ul>
+                                <li class="subitem1"><a href="#">Cute Kittens</a></li>
+                                <li class="subitem2"><a href="#">Strange “Stuff”</a></li>
+                                <li class="subitem3"><a href="#">Automatic Fails</a></li>
+                            </ul>
+                        </li>
+                    </ul>
 
-</div>
+                </div>
 
-<!--initiate accordion-->
-<script type="text/javascript">
-	$(function() {
-	
-	    var menu_ul = $('.menu > li > ul'),
-	           menu_a  = $('.menu > li > a');
-	    
-	    menu_ul.hide();
-	
-	    menu_a.click(function(e) {
-	        e.preventDefault();
-	        if(!$(this).hasClass('active')) {
-	            menu_a.removeClass('active');
-	            menu_ul.filter(':visible').slideUp('normal');
-	            $(this).addClass('active').next().stop(true,true).slideDown('normal');
-	        } else {
-	            $(this).removeClass('active');
-	            $(this).next().stop(true,true).slideUp('normal');
-	        }
-	    });
-	
-	});
-</script>
+                <!--initiate accordion-->
+                <script type="text/javascript">
+                    $(function () {
+
+                        var menu_ul = $('.menu > li > ul'),
+                                menu_a = $('.menu > li > a');
+
+                        menu_ul.hide();
+
+                        menu_a.click(function (e) {
+                            e.preventDefault();
+                            if (!$(this).hasClass('active')) {
+                                menu_a.removeClass('active');
+                                menu_ul.filter(':visible').slideUp('normal');
+                                $(this).addClass('active').next().stop(true, true).slideDown('normal');
+                            } else {
+                                $(this).removeClass('active');
+                                $(this).next().stop(true, true).slideUp('normal');
+                            }
+                        });
+
+                    });
+                </script>
             </aside>
         </section>
         <footer>
             <p>&copy; Puedes contactar con nosotros en el siguiente enlace | <a href="/" target="_blank" rel="nofollow">Contacto</a></p>
             <address>
-                Contacta vía E-mail-<a href="mailto:ethazi3@gmail.com">ethazi3@gmail.com</a>
+                Contacta vía E-mail | <a href="mailto:ethazi3@gmail.com">ethazi3@gmail.com</a>
             </address>
         </footer>   
-        <?php 
-            include 'footer.php';
-        ?>
 
-    </body>>
+
+    </body>
+</html>
