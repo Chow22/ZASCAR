fe328a41b1a3df094485af73379864ae3a10ee0d
Sergio <Sergio@192.168.32.117>
2018-01-15 10:43:45 +0100

    insertar trayectos corregido

--- public_html/modelo/trayectos.php
+++ public_html/modelo/trayectos.php
@@ -1,69 +1,73 @@
 <?php
+
 require_once '../conector/conector.php';
-class trayectos{
+
+class trayectos {
+
     private $link;
     private $usuario;
 
-    public function __construct(){
-        $this->link=Conectar::conexion();
-        $this->usuario=array();
-         }
-
-    public function get_trayectoConductor($idusu){
-       $sql="CALL verTrayectosConductor('$idusu')";
-       $consulta=$this->link->query($sql);
-         while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC))
-           {
-            $this->usuario[]=$row;
-           }
-       $consulta->free_result();
-       $this->link->close();
-       return $this->usuario;
-      }
-      
-      public function get_trayectoPasajero($idusu){
-       $sql="CALL verTrayectosPasajero('$idusu')";
-       $consulta=$this->link->query($sql);
-         while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC))
-           {
-            $this->usuario[]=$row;
-           }
-       $consulta->free_result();
-       $this->link->close();
-       return $this->usuario;
-      }
-      
-            public function get_trayectoPeticiones($idusu){
-       $sql="CALL verPeticiones('$idusu')";
-       $consulta=$this->link->query($sql);
-         while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC))
-           {
-            $this->usuario[]=$row;
-           }
-       $consulta->free_result();
-       $this->link->close();
-       return $this->usuario;
-      }
-       public function listar_trayectos(){
-       $sql="CALL listarTrayecto()";
-       $consulta=$this->link->query($sql);
-         while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC))
-           {
-            $this->usuario[]=$row;
-           }
-       $consulta->free_result();
-       $this->link->close();
-       return $this->usuario;
-      }
-      
-
-public function borrarTrayectoPasajero($id,$idusu){
-        $consulta=$this->link->query("CALL borrarTrayectoPasajero ('$id','$idusu')");
-
+    public function __construct() {
+        $this->link = Conectar::conexion();
+        $this->usuario = array();
     }
 
-public function aceptarPeticion($idtrayecto,$idusu){
-        $consulta=$this->link->query("CALL aceptarPeticion ('$idtrayecto','$idusu')");
+    public function get_trayectoConductor($idusu) {
+        $sql = "CALL verTrayectosConductor('$idusu')";
+        $consulta = $this->link->query($sql);
+        while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+            $this->usuario[] = $row;
+        }
+        $consulta->free_result();
+        $this->link->close();
+        return $this->usuario;
     }
- }
+
+    public function get_trayectoPasajero($idusu) {
+        $sql = "CALL verTrayectosPasajero('$idusu')";
+        $consulta = $this->link->query($sql);
+        while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+            $this->usuario[] = $row;
+        }
+        $consulta->free_result();
+        $this->link->close();
+        return $this->usuario;
+    }
+
+    public function get_trayectoPeticiones($idusu) {
+        $sql = "CALL verPeticiones('$idusu')";
+        $consulta = $this->link->query($sql);
+        while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+            $this->usuario[] = $row;
+        }
+        $consulta->free_result();
+        $this->link->close();
+        return $this->usuario;
+    }
+
+    public function listar_trayectos() {
+        $sql = "CALL listarTrayecto()";
+        $consulta = $this->link->query($sql);
+        while ($row = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+            $this->usuario[] = $row;
+        }
+        $consulta->free_result();
+        $this->link->close();
+        return $this->usuario;
+    }
+
+    public function borrarTrayectoPasajero($id, $idusu) {
+        $consulta = $this->link->query("CALL borrarTrayectoPasajero ('$id','$idusu')");
+    }
+
+    public function aceptarPeticion($idtrayecto, $idusu) {
+        $consulta = $this->link->query("CALL aceptarPeticion ('$idtrayecto','$idusu')");
+    }
+
+    public function insertar_trayecto($origen, $destino, $fechahora, $plazas, $paradas, $id) {
+        $consulta = $this->link->query("CALL sp_insertar_trayecto ('$origen','$destino','$fechahora','$plazas','$paradas','$id')");
+    }
+
+}
+
 ?>
